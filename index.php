f718946ec9470db993b8a09904ade435ccf7e694
LAB321 Student08 <lab321student08@gau.local>
2024-02-27 12:51:59 +0400

    commit1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz - 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div><h3>Questions</h3></div>
+            <div><h3>Answers</h3></div>
+            <div><h3>Grade</h3></div>
+        </div>
+        <div class="row">
+            <div>What is HTML?</div>
+            <div><input type="text" placeholder="answer"></div>
+            <div>7</div>
+        </div>
+        <div class="row">
+            <div>What is HTML?</div>
+            <div><input type="text" placeholder="answer"></div>
+            <div>7</div>
+        </div>
+        <div class="row">
+            <div>What is HTML?</div>
+            <div><input type="text" placeholder="answer"></div>
+            <div>7</div>
+        </div>
+        <div class="button">
+            <button>Send</button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ questions.php
@@ -0,0 +1,10 @@
+<?php
+$questions = [
+    ['question' => "What is HTML?", 'gr1' => 7],
+    ['question' => "What is PHP?", 'gr1' => 7],
+    ['question' => "What is JS?", 'gr1' => 7],
+    ['question' => "What is CSS?", 'gr1' => 7],
+    ['question' => "What is JAVA?", 'gr1' => 7]
+];
+
+?>
